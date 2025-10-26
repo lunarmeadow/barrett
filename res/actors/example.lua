ec27c207e5696c449f5bd2eec33c9d37b9f5c398
erysdren <contact@erysdren.me>
2025-10-26 12:17:36 -0500

    example.lua: update

--- res/actors/example.lua
+++ res/actors/example.lua
@@ -1,7 +1,13 @@
 -- example actor
 
 function actor:spawn()
-
+	-- replace tile indexes
+	self.replaceSpriteTile(108, 111) -- low guard
+	self.replaceSpriteTile(112, 115) -- low guard (patrol)
+	self.replaceSpriteTile(116, 119) -- low guard (ambush)
+	self.replaceSpriteTile(126, 129) -- hard mode low guard
+	self.replaceSpriteTile(130, 133) -- hard mode low guard (patrol)
+	self.replaceSpriteTile(134, 137) -- hard mode low guard (ambush)
 end
 
 function actor:think()
