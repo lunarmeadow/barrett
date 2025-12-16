606b268e894e2d46c003176e0eecd6b0720bcf07
Ashley Scott <ashley68k@proton.me>
2025-12-16 13:34:39 -0500

    HashTable.c: early work on png caching table

--- src/HashTable.c
+++ src/HashTable.c
@@ -6,6 +6,11 @@
 #define ARRAY_SIZE(x) (sizeof((x)) / sizeof((x)[0]))
 #endif
 
+
+// ===================================================
+// STANDARD HASH TABLE
+// ===================================================
+
 void InitHashTable(HashTable* hashTable, int initSize)
 {
 	hashTable->totalSize = initSize;
@@ -94,6 +99,122 @@ void Insert(HashTable* hashTable, int key, int item)
 int Lookup(HashTable* hashTable, int key)
 {
 	int index = HashFunc(hashTable, key);
+
+	int origIndex = index;
+
+	while (hashTable->table[index]->key != key &&
+		   hashTable->table[index]->isAvaliable == 0)
+	{
+		// go to next cell
+		++index;
+
+		// wrap around the table
+		index %= hashTable->totalSize;
+
+		if (index == origIndex)
+		{
+			return 0;
+		}
+	}
+
+	return hashTable->table[index]->data;
+}
+
+// ===================================================
+// PATCH CACHING TABLE
+// ===================================================
+
+void InitPatchTable(PatchTable* hashTable, int initSize)
+{
+	hashTable->totalSize = initSize;
+
+	hashTable->table = calloc(sizeof(patchNode), initSize);
+
+	patchNode* emptyNode = malloc(sizeof(patchNode));
+	
+	emptyNode->data->width = 0;
+	emptyNode->data->height = 0;
+	emptyNode->data->leftoffset = 0;
+	emptyNode->data->topoffset = 0;
+	emptyNode->data->origsize = 0;
+
+	emptyNode->size = 0;
+
+	int x;
+
+	for (x = 0; x < initSize; x++)
+	{
+		patchNode* node = malloc(sizeof(patchNode));
+
+		memcpy(node, emptyNode, sizeof(&emptyNode));
+
+		hashTable->table[x] = node;
+	}
+
+	free(emptyNode);
+}
+
+int HashPatchFunc(PatchTable* hashTable, int key)
+{
+	return abs((key)) % hashTable->totalSize;
+}
+
+void PatchDelete(PatchTable* hashTable, int key)
+{
+	int index = HashPatchFunc(hashTable, key);
+
+	while (hashTable->table[index]->key != key)
+	{
+		// go to next cell
+		++index;
+
+		// wrap around the table
+		index %= hashTable->totalSize;
+	}
+
+	// LinkedList * list = hashTable->table[index];
+
+	// DeleteWithKey(list, key);
+	// free(&hashTable->table[index]);
+	hashTable->table[index]->isAvaliable = 1;
+}
+
+void ClearPatchTable(PatchTable* hashTable)
+{
+	int x = 0;
+
+	// free all table entries until last index.
+	// do allows zero-case to be covered,
+	// pre-increment ensures that last case isn't skipped
+	do free(hashTable->table[x]);
+		while(hashTable->table[++x] != NULL);
+
+	free(hashTable->table);
+}
+
+void InsertPatch(PatchTable* hashTable, int key, decode_patch_t* item)
+{
+	int index = HashPatchFunc(hashTable, key);
+
+	while (hashTable->table[index]->isAvaliable == 0)
+	{
+		// go to next cell
+		++index;
+
+		// wrap around the table
+		index %= hashTable->totalSize;
+	}
+
+	// listNode * newNode = malloc(sizeof(listNode));
+
+	hashTable->table[index]->data = item;
+	hashTable->table[index]->key = key;
+	hashTable->table[index]->isAvaliable = 0;
+}
+
+decode_patch_t* LookupPatch(PatchTable* hashTable, int key)
+{
+	int index = HashPatchFunc(hashTable, key);
 
 	int origIndex = index;
 
--- src/HashTable.h
+++ src/HashTable.h
@@ -12,6 +12,8 @@
 #include "rt_def.h"
 #include "lumpy.h"
 
+#include "w_kpfdat.h"
+
 typedef struct listNode
 {
 	int isAvaliable;
@@ -19,30 +21,44 @@ typedef struct listNode
 	int data;
 } listNode;
 
-typedef struct patchNode
-{
-	int isAvaliable;
-	char* key;
-	patch_t* data;
-	int size;
-} patchNode;
-
 typedef struct HashTable
 {
 	int totalSize;
 	listNode** table;
 } HashTable;
 
+
+typedef struct patchNode
+{
+	int isAvaliable;
+	int key;
+	decode_patch_t* data;
+	int size;
+} patchNode;
+
+typedef struct PatchTable
+{
+	int totalSize;
+	patchNode** table;
+} PatchTable;
+
+// STANDARD HASH TABLE
+
 void InitHashTable(HashTable* hashTable, int initSize);
-
 int HashFunc(HashTable* hashTable, int key);
-
 void Delete(HashTable* hashTable, int key);
-
 void ClearHashTable(HashTable* hashTable);
-
 void Insert(HashTable* hashTable, int key, int item);
-
 int Lookup(HashTable* hashTable, int key);
+
+// PATCH CACHING TABLE
+
+void InitPatchTable(PatchTable* hashTable, int initSize);
+int HashPatchFunc(PatchTable* hashTable, int key);
+void DeletePatch(PatchTable* hashTable, int key);
+void ClearPatchTable(PatchTable* hashTable);
+void InsertPatch(PatchTable* hashTable, int key, decode_patch_t* item);
+decode_patch_t* LookupPatch(PatchTable* hashTable, int key);
+
 
 #endif /* HASHTABLE_H */
