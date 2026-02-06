472e8d036cfaeceaa46b9415bbb6170b654d35a0
Ashley Scott <ashley68k@proton.me>
2026-02-06 17:42:25 -0500

    Update copyright headers in source files

--- src/fx_man.h
+++ src/fx_man.h
@@ -3,8 +3,8 @@
 Copyright (C) 1994-1995 Apogee Software, Ltd.
 Copyright (C) 2002-2015 icculus.org, GNU/Linux port
 Copyright (C) 2017-2018 Steven LeVesque
-Copyright (C) 2025 lunarmeadow (she/her)
-Copyright (C) 2025 erysdren (it/its)
+Copyright (C) 2025-2026 lunarmeadow (she/her)
+Copyright (C) 2025-2026 erysdren (it/its)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
